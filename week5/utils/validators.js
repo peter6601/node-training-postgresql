6f1b9640a4d3780ae8cf39f574679c521ae8e5e6
DinDin <peter779701@gmail.com>
2025-02-20 12:28:57 +0800

    [Add] 加入共同methods isUndefined, isNotValidString, isNotValidInteger

---
+++ week5/utils/validators.js
@@ -0,0 +1,16 @@
+ const validators = {
+    isUndefined(value) {
+      return value === undefined;
+    },
+  
+    isNotValidString(value) {
+      return typeof value !== "string" || value.trim().length === 0 || value === "";
+    },
+  
+    isNotValidInteger(value) {
+      return typeof value !== "number" || value < 0 || value % 1 !== 0;
+    }
+  };
+
+  module.exports = validators;
+
