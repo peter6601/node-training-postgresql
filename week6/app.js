030c374429e9716026f4ec21e46e8216b2ede3b9
DinDin <peter779701@gmail.com>
2025-02-24 14:10:54 +0800

    [Build] 創建 week6

---
+++ week6/app.js
@@ -0,0 +1,54 @@
+const express = require('express')
+const cors = require('cors')
+const path = require('path')
+const pinoHttp = require('pino-http')
+
+const logger = require('./utils/logger')('App')
+const creditPackageRouter = require('./routes/creditPackage')
+const skillRouter = require('./routes/skill')
+const userRouter = require('./routes/user')
+const adminRouter = require('./routes/admin')
+const coachRouter = require('./routes/coach')
+
+const creditPackagePath = "/api/credit-package"
+const skillPath = "/api/coaches/skill"
+const userPath = "/api/users"
+const adminPath = "/api/admin"
+const coachnPath = "/api/coaches"
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(pinoHttp({
+  logger,
+  serializers: {
+    req (req) {
+      req.body = req.raw.body
+      return req
+    }
+  }
+}))
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.get('/healthcheck', (req, res) => {
+  res.status(200)
+  res.send('OK')
+})
+app.use(creditPackagePath, creditPackageRouter)
+app.use(skillPath, skillRouter)
+app.use(userPath, userRouter)
+app.use(adminPath, adminRouter)
+app.use(coachnPath, coachRouter)
+
+
+// eslint-disable-next-line no-unused-vars
+app.use((err, req, res, next) => {
+  req.log.error(err)
+  res.status(500).json({
+    status: 'error',
+    message: '伺服器錯誤'
+  })
+})
+
+module.exports = app
---
+++ week6/routes/creditPackage.js
@@ -0,0 +1,72 @@
+const express = require('express')
+const router = express.Router()
+const { dataSource } = require('../db/data-source')
+const logger = require('../utils/logger')('CreditPackage')
+const {isUndefined, isNotValidString, isNotValidInteger } = require('../utils/validators')
+const { sendSuccessResponse, sendFailResponse } = require('../utils/responseHandler')
+
+
+router.get('/', async (req, res, next) => {
+    try {
+        const packages = await dataSource.getRepository("CreditPackage").find({
+          select: ["id", "name", "credit_amount", "price"]
+        })
+        sendSuccessResponse(res, 200, packages)
+      } catch (error) {
+        logger.error(error)
+        next(error)    
+      }
+})
+
+router.post('/', async (req, res, next) => {
+    try {
+        const data = req.body
+        if (isUndefined(data.name) || isNotValidString(data.name) ||
+          isUndefined(data.credit_amount) || isNotValidInteger(data.credit_amount) ||
+          isUndefined(data.price) || isNotValidInteger(data.price)) {
+            sendFailResponse(res, 400, "欄位未填寫正確")
+          return
+        }
+        const creditPackageRepo = await dataSource.getRepository("CreditPackage")
+        const existPackage = await creditPackageRepo.find({
+          where: {
+            name: data.name
+          }
+        })
+        if (existPackage.length > 0) {
+             sendFailResponse(res, 409, "資料重複")
+          return
+        }
+        const newPackage = await creditPackageRepo.create({
+          name: data.name,
+          credit_amount: data.credit_amount,
+          price: data.price
+        })
+        const result = await creditPackageRepo.save(newPackage)
+        sendSuccessResponse(res,200, result)
+      } catch (error) {
+        logger.error(error)
+        next(error)
+      }
+})
+
+router.delete('/:creditPackageId', async (req, res, next) => {
+    try {
+        const {creditPackageId} = req.params
+        if (isUndefined(creditPackageId) || isNotValidString(creditPackageId)) {
+            sendFailResponse(res, 400, '欄位未填寫正確')
+          return
+        }
+        const result = await dataSource.getRepository("CreditPackage").delete(creditPackageId)
+        if (result.affected === 0) {
+            sendFailResponse(res, 400, 'ID錯誤')
+          return
+        }
+        sendSuccessResponse(res, 200)
+      } catch (error) {
+        logger.error(error)
+        next(error)
+      }
+})
+
+module.exports = router
