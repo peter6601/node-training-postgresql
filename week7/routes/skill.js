0b7b046f767aed76b4e96f09fcf2ac4fb8246e99
DinDin <peter779701@gmail.com>
2025-03-04 16:24:57 +0800

    [Update] 把 route skill API邏輯搬到 controller底下

--- week7/routes/skill.js
+++ week7/routes/skill.js
@@ -1,69 +1,12 @@
 const express = require('express')
 const router = express.Router()
-const { dataSource } = require('../db/data-source')
-const logger = require('../utils/logger')('skill')
-const {isUndefined, isNotValidString, isNotValidInteger } = require('../utils/validators')
-const { sendSuccessResponse, sendFailResponse } = require('../utils/responseHandler')
-const repoName = "Skill"
-
-router.get('/', async (req, res, next) => {
-    try  {
-        let list = await dataSource.getRepository(repoName).find({
-            select: ["id", "name", "createdAt"]
-        })
-        sendSuccessResponse(res,200, list)
-    } catch(error) {
-        logger.error(error)
-        next(error)
-    }
-})
-
-router.post('/', async (req, res, next) => {
-    try {
-        const data = req.body
-        if (isUndefined(data.name) || isNotValidString(data.name)) {
-            sendFailResponse(res, 400, "欄位未填寫正確")
-          return
-        }
-        const skillPackagesRepo = await dataSource.getRepository(repoName)
-        const exitPackages = await skillPackagesRepo.find({
-          where: {
-            name: data.name
-          }
-        })
-        if (exitPackages.length > 0) {
-            sendFailResponse(res, 409, "資料重複")
-          return
-        }
-        const newPackages = await skillPackagesRepo.create({
-          name: data.name
-        })
-        const result = await skillPackagesRepo.save(newPackages)
-        sendSuccessResponse(res,200, result)
-      } catch (error) {
-        logger.error(error)
-        next(error)
-      }
-})
+const skills = require('../controllers/skill')
 
 
-router.delete('/:skillID', async (req, res, next)=> {
-    try {
-        const {skillID} = req.params
-        if (isUndefined(skillID) || isNotValidString(skillID)) {
-            sendFailResponse(res, 400, "ID錯誤")
-          return 
-        }
-        let result = await dataSource.getRepository(repoName).delete(skillID)
-        if (result.affected === 0) {
-            sendFailResponse(res, 400, "ID錯誤")
-          return
-        }
-        sendSuccessResponse(res, 200)
-      } catch (error) {
-        logger.error(error)
-        next(error)
-      }
-})
+router.get('/', skills.getAll)
+
+router.post('/', skills.postNew)
+
+router.delete('/:skillID', skills.deleteOne)
 
 module.exports = router
